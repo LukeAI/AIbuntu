3c323c25d5c67241e73d2795e8ecf214649bf753
Luke Taylor <luke@aidrivers.ai>
2018-12-05 13:31:29 +0000

    ros install error

--- setupscript.sh
+++ setupscript.sh
@@ -100,7 +100,7 @@ conda create --name pytorch -y && source activate pytorch && conda install -c an
 
 # Install ROS
 echo 'downloading, building and installing ROS'
-sudo echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" >> /etc/apt/sources.list.d/ros-latest.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update | tee -a ~/install_logs/ros.log
 sudo apt-get install ros-kinetic-desktop-full -y | tee -a ~/install_logs/ros.log
